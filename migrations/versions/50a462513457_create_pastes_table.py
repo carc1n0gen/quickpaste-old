33fe1924857ae6e7e4027a97984f3ec6dd445396
Carson Evans <carson_evans@manulife.com>
2019-01-17 12:23:48 -0500

    Added database migrations and fixed css issue where textarea spilled outside of right and bottom edge

---
+++ migrations/versions/50a462513457_create_pastes_table.py
@@ -0,0 +1,29 @@
+"""create pastes table
+
+Revision ID: 50a462513457
+Revises: 
+Create Date: 2019-01-17 12:10:06.961637
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '50a462513457'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('pastes',
+        sa.Column('hash', sa.Binary(16)),
+        sa.Column('text', sa.Text()),
+        sa.Column('timestamp', sa.TIMESTAMP()),
+        sa.PrimaryKeyConstraint('hash')
+    )
+
+
+def downgrade():
+    op.drop_table('pastes')
