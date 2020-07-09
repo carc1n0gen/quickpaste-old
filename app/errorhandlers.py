19716b2c4517b35c185da9f5498a65329d9ca89e
Carson Evans <carsonwevans@gmail.com>
2020-07-09 11:41:08 -0400

    Fixed error handlers with new template locations

--- app/errorhandlers.py
+++ app/errorhandlers.py
@@ -16,6 +16,10 @@ not_found_text = """# Not Found
 
 **There doesn't seem to be anything here.**"""
 
+rate_limit_text = """# Too Many Requests
+
+**Limit: {}**"""
+
 
 def setup_handlers(app):
     @app.errorhandler(400)
@@ -24,38 +28,23 @@ def setup_handlers(app):
         return dict(
             status=400,
             message='400 missing text',
-            url=url_for('edit.edit', _external=True)
+            url=url_for('paste.edit', _external=True)
         )
 
     @app.errorhandler(404)
     def not_found(ex):
         return render_template(
-            'view/view.html',
+            'paste_show.html',
             text=highlight(not_found_text, MarkdownLexer(), HtmlFormatter()),
             lines=not_found_text.count('\n') + 1,
             status=404
         )
 
-    @app.errorhandler(413)
-    def too_large(ex):
-        text = f"""
-# Too many characters
-
-Limit: {app.config['MAX_PASTE_LENGTH']}"""
-        return render_template(
-            'view/view.html',
-            text=highlight(text, MarkdownLexer(), HtmlFormatter()),
-            lines=text.count('\n') + 1,
-        ), 413
-
     @app.errorhandler(429)
     def rate_limit(ex):
-        text = f"""
-# Too Many Requests
-
-**Limit: {app.config.get('RATELIMIT_DEFAULT')}**"""
+        text = rate_limit_text.format(app.config.get('RATELIMIT_DEFAULT'))
         return render_template(
-            'view/view.html',
+            'paste_show.html',
             text=highlight(text, MarkdownLexer(), HtmlFormatter()),
             lines=text.count('\n') + 1
         ), 429
@@ -77,7 +66,7 @@ Limit: {app.config['MAX_PASTE_LENGTH']}"""
 
             app.logger.exception(f'Unknown error at endpoint: {request.full_path}')
             return render_template(
-                'view/view.html',
+                'paste_show.html',
                 text=highlight(unknown_error_text, MarkdownLexer(), HtmlFormatter()),
                 lines=unknown_error_text.count('\n') + 1
             ), 500
