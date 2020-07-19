a39dcc8c60992fcdc378c6277a7592f7d7ff879d
Carson Evans <carsonwevans@gmail.com>
2020-07-19 18:09:30 -0400

    Create abort_if helper

--- app/views/paste_raw.py
+++ app/views/paste_raw.py
@@ -1,6 +1,6 @@
-from flask import abort
 from flask.views import View
 from app.repositories import paste
+from app.helpers import abort_if
 
 
 class PasteRaw(View):
@@ -8,8 +8,7 @@ class PasteRaw(View):
 
     def dispatch_request(self, id, extension=None):
         doc = paste.get_paste(id)
-        if doc is None:
-            abort(404)
+        abort_if(doc is None, 404)
 
         return (doc['text'], 200, {
             'Content-type': 'text/plain; charset=utf-8',
