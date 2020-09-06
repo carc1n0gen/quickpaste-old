374e06a37f03d3c9c51d875835f0fd4494ac039d
Carson Evans <carsonwevans@gmail.com>
2020-09-06 16:57:40 -0400

    Separate app and client to their own fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,14 @@ from app.create_app import create_app
 
 
 @pytest.fixture
-def client():
+def app():
     app = create_app()
     app.config['TESTING'] = True
+    app.debug = True
     app.config['WTF_CSRF_ENABLED'] = False
+    yield app
+
+
+@pytest.fixture
+def client(app):
     yield app.test_client()
