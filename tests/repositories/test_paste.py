04f24c3185e5d0f0fae05bd7f41cb7d3d003d5c9
Carson Evans <carsonwevans@gmail.com>
2020-09-06 23:48:08 -0400

    Add past repository test case for no doc found

--- tests/repositories/test_paste.py
+++ tests/repositories/test_paste.py
@@ -10,3 +10,11 @@ def test_get_paste_happy_path(app, monkeypatch):
         doc = paste.get_paste('abcd')
         assert doc is not None
         assert doc['_id'] == 'abcd'
+
+
+def test_get_paste_sad_path(app, monkeypatch):
+    monkeypatch.setattr(repositories, 'MongoClient', MockMongoClient)
+
+    with app.app_context():
+        doc = paste.get_paste('1234')
+        assert doc is None
